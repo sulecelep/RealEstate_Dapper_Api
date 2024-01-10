2ceac62617ea9d3d291c35164a6137bb4cd748eb
ssule <ssulecelepp@gmail.com>
2024-01-10 19:04:43 +0300

    53. ders sonu. Signal R ile veri getirildi.

--- RealEstate_Dapper_Api/Hubs/SignalRHub.cs
+++ RealEstate_Dapper_Api/Hubs/SignalRHub.cs
@@ -1,8 +1,23 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace RealEstate_Dapper_Api.Hubs
 {
-    public class SignalRHub:Hub
+    public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task SendCategoryCount() //Kanal gibi düşünelim abone olucaz 
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage6 = await client.GetAsync("https://localhost:44352/api/Statistics/CategoryCount");
+            var value = await responseMessage6.Content.ReadAsStringAsync();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
     }
 }
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddCors(opt =>
                 .AllowCredentials();
     });
 });
+builder.Services.AddHttpClient();
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
